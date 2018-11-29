c81531d0afce7d53de06ffd0254f8db13384931e
DESKTOP-MFQCCVU\yazid <yazid@decision-science.com>
2018-11-29 11:44:22 +0800

    Add timmer modal

--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
     selector: 'app-stop-training',
     template: `<h1 mat-dialog-title>Are you sure?</h1>
+               <mat-dialog-content>You already got {{ this.passedData.progress }}%</mat-dialog-content>
                <mat-dialog-actions>
                    <button mat-button [mat-dialog-close]="true">Yes</button>
                    <button mat-button [mat-dialog-close]="false">No</button>
                </mat-dialog-actions>`
 })
 
-export class StopTrainingComponent {}
+export class StopTrainingComponent {
+    //  inject an object
+    constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
+}
