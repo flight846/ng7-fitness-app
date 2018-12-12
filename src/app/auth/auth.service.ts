3d805c559318267a3df2dd6bcdbc6988bd064878
DESKTOP-MFQCCVU\yazid <yazid@decision-science.com>
2018-12-12 14:30:36 +0800

    Add redirect on auth success

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
 
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 
+@Injectable()
 export class AuthService {
     authChange = new Subject<boolean>();
     private user: User;
+
+    constructor(private router: Router) {}
 
     registerUser(authData: AuthData) {
         this.user = {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccess();
     }
 
     login(authData: AuthData)  {
@@ -20,12 +25,13 @@ export class AuthService {
             email: authData.email,
             userId: Math.round(Math.random() * 10000).toString()
         };
-        this.authChange.next(true);
+        this.authSuccess();
     }
 
     logout() {
         this.user = null;
         this.authChange.next(false);
+        this.router.navigate(['/login']);
     }
 
     getUser() {
@@ -34,5 +40,10 @@ export class AuthService {
 
     isAuth() {
         return this.user != null;
+    }
+
+    private authSuccess() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 }
